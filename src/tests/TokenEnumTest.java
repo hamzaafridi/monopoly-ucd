ceb1cc26701b53d2745d41d385abcfbc47c93933
Hamza Afridi <hamza.cykiq@gmail.com>
2019-11-07 12:14:08 +0000

    moved token test to the test package

--- src/tests/TokenEnumTest.java
+++ src/tests/TokenEnumTest.java
@@ -1,8 +1,10 @@
-package utils;
+package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import utils.Token;
 /**
  * This is a JUnit test for Token Enum class
  * @author Hamza Afridi
@@ -15,7 +17,7 @@ public abstract class TokenEnumTest {
 	/**
 	 * Tests for correct implementation of Enum class.
 	 */
-	void test() {
+	void testTokenInstance() {
 		Token player1 = Token.BLACK;
 		assertEquals(Token.valueOf("BLACK"),player1);
 	}
