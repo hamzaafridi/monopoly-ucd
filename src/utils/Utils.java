ce200306ca0ea18fc02cfe40160a6a071e5fd693
Hamza Afridi <hamzaafridi@gmail.com>
2019-12-06 12:05:25 +0000

    fixed input stream! it is working and controlled with exception handling

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,2 +1,36 @@
 package utils;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Utils {
+	Scanner input;
+	
+	public Utils(Scanner in)
+	{
+		this.input = in;
+	}
+	
+	public int inInteger() {
+		int numUsers = -1;
+		
+		if (input.hasNextInt()) {
+	        numUsers = input.nextInt() ; 
+	      
+	    } else {
+	        System.out.println("\nPlease input integer only!");
+	        input.nextLine();
+	    }
+		
+		return numUsers;
+	}
+	
+	public void pressAnyKey() {
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		input.nextLine();
+	}
+}
